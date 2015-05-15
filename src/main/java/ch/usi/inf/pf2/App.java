f6ad59e6432d1bb952ed0cbad0dd1cfa94e703ba
Matthias Hauswirth <Matthias.Hauswirth@usi.ch>
2015-05-15 14:51:28 +0200

    turned into Maven project

---
+++ src/main/java/ch/usi/inf/pf2/App.java
@@ -0,0 +1,13 @@
+package ch.usi.inf.pf2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
